f07cb0127461b449921178b2d349ce7725106d8b
IlmanNafi11 <ilmannafi04@gmail.com>
2024-08-15 23:33:38 +0700

    code ui navbar, about, and service content

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+// script.js
+document.getElementById('toggle-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
+    const navMenu = document.getElementById('nav-menu');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    if (this.classList.contains('active')) {
+        navMenu.classList.add('open');
+        navItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.style.opacity = 1;
+            }, (index + 1) * 200);
+        });
+
+    } else {
+        navItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.style.opacity = 0;
+            }, (navItems.length - index) * 200);
+        });
+
+        setTimeout(() => {
+            navMenu.classList.remove('open');
+        }, navItems.length * 200 + 300);
+    }
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,59 +1,122 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    {{-- Style --}}
-    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    {{-- Bootstrap Style --}}
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    {{-- Custom Style --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    {{-- font --}}
+    <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
     <title>Ilman | Home</title>
 </head>
+
 <body>
     {{-- Container --}}
     <div class="container-fluid p-0 m-0 main-container">
-        <nav class="navbar fixed-top p-0 m-0">
-            <div class="container-fluid justify-content-between px-0 py-0">
-                <div class="main-logo">
-                    <a href="#" class="navbar-brand text-white align-items-center">ILMAN</a>
-                </div>
-                <div class="toggle-menu">
-                    <span></span>
-                    <span></span>
-                </div>
+        <nav class="navbar fixed-top m-0 justify-content-between px-3 py-0">
+            <div class="main-logo">
+                <a href="#" class="navbar-brand text-white">ILMAN</a>
+            </div>
+            <div class="toggle-menu" id="toggle-menu">
+                <span class="line-hamburger"></span>
+                <span class="line-hamburger"></span>
+            </div>
+            <div class="nav-menu bg-dark d-flex flex-column justify-content-center align-items-center" id="nav-menu">
+                <ul class="list-unstyled text-center">
+                    <li class="nav-item mb-2">
+                        <a class="text-decoration-none" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a class="text-decoration-none" href="#">About</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a class="text-decoration-none" href="#">Service</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a class="text-decoration-none" href="#">Project</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a class="text-decoration-none" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
         </nav>
         {{-- Navbar --}}
 
 
         {{-- Content --}}
-        <div class="container-content">
+        <div class="container-fluid bg-dark container-content px-0 py-0">
 
             {{-- hero --}}
-            <div class="content hero-images container-fluid m-0 p-0">
-                <div class="hero-title d-flex flex-column align-items-center justify-content-center text-white">
-                    <p class="hero main-title">Ilman Nafin</p>
-                    <p class="hero subtitle">Web developer since 2024 in sragen, Indonesia</p>
+            <div class="section hero-section d-flex justify-content-center align-items-center">
+                <span class="text-uppercase text-white text-center fw-bold">Ilman Nafian <br>Mobile and web Developer
+                    since 2024 in sragen, Indonesia</span>
+            </div>
+
+            <div class="section content-section d-flex px-3 py-4">
+                <div class="about-images pe-4">
+                    <img src="{{asset('img/its-me.jpg')}}" alt="my-foto" class="object-fit-cover w-100 h-100 d-block rounded">
+                </div>
+                <div class="content-section-about">
+                    <span class="header-content-section">About Me carrer</span>
+                    <p class="main-section-content text-white">My name is Ilman Nafian. I'am Mobile Developer and Web Developer from Sragen, Indonesia</p>
+                    <p class="subtitle-section-about text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel deserunt officiis, ratione eaque alias recusandae maiores nam porro delectus nisi quasi nulla esse iste sint obcaecati earum praesentium eius quam?</p>
+                    <p class="subtitle-section-about text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam id facere aperiam ipsa, quod eius molestias explicabo, repellendus architecto nostrum suscipit. Accusantium, fugit a saepe itaque quas eligendi quidem quam.</p>
+                    <img src="{{asset('img/my-sign.png')}}" alt="sign" class="d-block w-25 h-auto mb-4 mt-0 mw-100 mh-auto">
+                    <button class="btn btn-light d-block">Download CV</button>
                 </div>
             </div>
 
-            {{-- about section --}}
-            <div class="section section-about d-flex mt-5">
-                <div class="about about-images">
-                    <img src="{{asset('img/card-1.jpg')}}" alt="my-foto">
-                </div>
-                <div class="about about-content">
-                    <h4 class="title-about title-about-content">about my carrer</h4>
-                    <h3 class="main-about main-about-content">My name is Ilman Nafian. I'm Mobile developer and web developer from sragen, indonesia.</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur recusandae mollitia officiis sint obcaecati similique atque in eum. Libero nisi dolore, dignissimos dolores error blanditiis necessitatibus minima omnis laboriosam molestias.</p>
+            <div class="section service-section px-3 py-4">
+                <span class="header-content-section">awesome service</span>
+                <p class="main-section-content text-white">I recommed you high quality service</p>
+                <div class="group-box-service">
+                    <div class="container-fluid d-flex flex-row row-gap-4 flex-wrap justify-content-between px-0 py-0">
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                        <div class="service-item rounded p-4">
+                            <span class="header-service text-white">Web Development</span>
+                            <p class="subtitle-service text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id unde nostrum odit incidunt delectus? Maiores quibusdam soluta distinctio, beatae cumque eaque, iste ducimus tempora neque commodi dicta earum totam expedita.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-
     </div>
-    <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    {{-- Bootstrap js --}}
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    {{-- custom js --}}
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
+
 </html>
